03d6f10fee04f2ea489ca817b99929889ce50f3a
Jenushan Jayarooban <133542213+jenu-21@users.noreply.github.com>
2024-05-02 14:35:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
-# Convert the Snowspark Dataframe to a pandas datafram so we can use the LOC function
+# Convert the Snowspark Dataframe to a pandas dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
@@ -39,11 +39,15 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        
+        if search_on is not None:  # Check if search_on is not None
+            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        else:
+            st.error(f"Search value not found for {fruit_chosen}.")
 
 
     # st.write(ingredients_string)
